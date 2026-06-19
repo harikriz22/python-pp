d9c74aa0f608e0723cdd351161e344eab1e9b36a
Harikrishnan <54831389+harikriz22@users.noreply.github.com>
2026-06-19 20:43:04 +0530

    Add files via upload

---
+++ four.py
@@ -0,0 +1,300 @@
+# # # # # # # arithemtic operators
+# # # # # # # bool datatypes
+# # # # # # # comparison operators
+# # # # # # # logical operators
+# # # # # # # conidtional statements
+# # # # # # #     if statement
+# # # # # # #     if else
+# # # # # # #     elif
+
+
+# # # # # # # arithemtic operators
+
+# # # # # # # addition(+)
+# # # # # # a = 7
+# # # # # # b = 11.10
+
+# # # # # # print(a + b)
+
+# # # # # # # subtraction (-)
+
+# # # # # # a = 11
+# # # # # # b = 17.6
+# # # # # # print(a - b)
+
+# # # # # # # multiplication(*)
+
+# # # # # # a = -4
+# # # # # # b = 6.6
+# # # # # # print(a * b)  # axb
+# # # # # # # division(/)
+
+# # # # # # a = 9
+# # # # # # b = 4
+
+# # # # # # print(a / b)
+
+# # # # # # # exponent (**)
+# # # # # # print(5**2)
+# # # # # # print(3**3)
+
+# # # # # # # floor division(//)
+
+
+# # # # # # print(9//4)
+
+
+# # # # # # # modulus(%)
+
+# # # # # # print(7%4)
+
+
+# # # # # # bool
+
+# # # # # a = True
+# # # # # print(type(a))
+# # # # # # print(bool(0))
+# # # # # print(bool(''))
+# # # # # b = False
+# # # # # # print(type(b))
+
+# # # # # =
+
+# # # # # # comparison operators
+
+# # # # # equall to (==)
+# # # # #
+# # # # # a = 6
+# # # # # b = 6
+
+# # # # # print(a == b)
+
+# # # # # not equall to (!=)
+
+# # # # # a = 9
+# # # # # b = 9
+# # # # # print(a != b)
+
+# # # # # greater than >
+# # # # # less than <
+
+# # # # a = 1
+# # # # b = 1
+
+# # # # # print(a >= b)
+# # # # print(a <= b)
+
+# # # # # greater than >=
+# # # # # less than <=
+
+# # # # # ==
+# # # # # !=
+# # # # # >
+# # # # # <
+# # # # # >=
+# # # # <=
+
+# # # a = 6
+# # # b = 6
+
+# # # print(a >= b)
+# # # print(a <= b)
+
+# # # conditional statement
+# # #     if statement
+# # #    if else
+# # #     elif
+
+
+# # # if
+
+# # # if condition :
+# # # code to be executed
+
+# # # indentation
+
+# # # age = 18
+# # # # print(age>18)
+# # # if age >= 18 :
+# # #     print('eligible to vote')
+# # # else:
+# # #     print('not eligible')
+
+# # # check if a number is even or odd
+# # # num = 7
+# # # largest of 2 number
+
+# # # num = 12
+
+# # # if num % 2 == 0 :
+# # #     print('even')
+# # # else:
+# # #     print('odd')
+
+
+# # # a = 6
+# # # b = 11
+
+# # # if a > b:
+# # #     print('a is largest')
+# # # else:
+# # #     print('b is largest')
+
+
+# # # logical operators
+# # # elif
+
+
+# # # mark = 34
+
+
+# # # if mark >= 60:
+# # #     print('B')
+# # # elif mark >= 50:
+# # #     print('C')
+# # # elif mark >= 40:
+# # #     print('pass')
+# # # else:
+# # #     print('fail')
+
+# # # check if a number is positive negative or zero
+
+
+# # a = 'mohan'
+# # b = "das"
+
+# # print(a+b)
+
+
+# # # addition +
+# # a = 5
+# # b = 7
+
+
+# # a = 8
+# # b = 8
+# # print(a+b)
+# # print(a-b)
+# # print(a*b)
+
+# # if
+# # if else
+# # elif
+
+# # logical operator
+
+# # logical operator
+
+# # or
+# # and
+# # not
+
+# # a = 10
+
+# # if a > 1 and a == 6:
+# #     print('yes')
+# # else:
+# #     print('no')
+
+# # if a > 15 or a == 8 or a == 1 or a < -100:
+# #     print('yes')
+
+# # OR
+
+# # True False -- True
+# # False True - True
+# # False False -- False
+# # True True   - True
+
+
+# # and
+
+# # True False -- False
+# # False True - False
+# # False False -- False
+# # True True   - True
+
+
+# # largest of 2 numbers
+
+
+# # largest of 3 numbers
+
+# # not
+
+# # a = 511
+# # b = 10
+# # c = 3
+
+# # if a > b and a > c :
+# #     print("a is largest")
+# # elif b > a and b > c:
+# #     print("b is largest")
+# # else:
+# #     print('c is largest')
+
+
+# # input()
+
+# # control statement
+# # loops
+
+# # while loop
+
+
+# # while condition:
+# # code to be executed
+
+
+# i = 1
+# sum = 0
+# while i < 11:
+#     sum = sum + i#s = 10
+#     i = i + 1
+# print(sum)
+
+# # print sum of odd numbers and even numbers from first 100 numbers
+
+# # 2050
+# # 2500
+
+
+# # sum of first 10 numbers
+
+# # ctrl + c
+
+
+# # print('hari')
+# # 1
+# # 2
+# # 3
+# # 4
+# # inifite times
+
+# i = 1
+# esum = 0 
+# osum = 0
+# while i <= 100:
+#     if i % 2 == 0 :
+#         esum = esum + i
+#     else:
+#         osum = osum + i 
+#     i = i + 1
+# print(esum, 'evensum')
+# print(osum,"oddsum")
+
+# num = 15432
+# 1 + 2 + 3 + 4 + 5 -- > 15 
+# 15 
+
+# print() - user output 
+# input() - user input 
+
+
+a = float(input("enter a number : - "))
+b = float(input("enter a number : - "))
+print(a+b)
+
+# '10'  -->  10
+# "hari"  --> ?
+# type conversion / type casting 
